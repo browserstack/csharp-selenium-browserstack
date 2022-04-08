e1de79fd66009cfd1ce04bd7bc72700205664137
RutvikChandla <rutvik.c@browserstack.com>
2022-04-08 09:50:22 +0530

    selenium - 3 changes

---
+++ csharp-selenium-browserstack/CSharpTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace csharp_selenium_browserstack
+{
+    public class CSharpTests
+    {
+        static void Main(string[] args)
+        {
+            //SingleTest.execute();
+            //ParallelTests.execute();
+            LocalTest.execute();
+        }
+    }
+}
