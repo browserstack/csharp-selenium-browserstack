20219d7c4978a0c4dc89d622cfeb38af797a0ed1
Neha Agarwal <neha.a@browserstack.com>
2023-07-10 12:02:31 +0530

    Corrected source

--- SingleTest.cs
+++ SingleTest.cs
@@ -26,7 +26,7 @@ namespace csharp_selenium_browserstack
             browserstackOptions.Add("local", "false");
             browserstackOptions.Add("buildName", "browserstack-build-1");
             browserstackOptions.Add("sessionName", "BStack single test");
-            browserstackOptions.Add("source", "csharp:sample-main:v1.0");
+            browserstackOptions.Add("source", "csharp:sample-sdk:v1.0");
             browserstackOptions.Add("userName", BROWSERSTACK_USERNAME);
             browserstackOptions.Add("accessKey", BROWSERSTACK_ACCESS_KEY);
             capabilities.AddAdditionalOption("bstack:options", browserstackOptions);
